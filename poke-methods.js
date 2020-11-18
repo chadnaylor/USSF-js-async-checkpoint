3ffd1d010a2c56e7ad2d4fc3f19e52434727f34c
Chad Naylor <chad.naylor@gmail.com>
2020-11-18 15:44:01 -0600

    methods file wasn't added to git

---
+++ poke-methods.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path')
+var getTypes = () => {
+    if (!process.argv[2]) {
+        throw Error('NO INPUT SPECIFIED.')
+    }
+    let data = fs.readFileSync(process.argv[2], 'utf8')
+    let pokemonNames = data.split('\n')
+    let requests = []
+    let result
+    return new Promise((resolve, reject) => {
+        pokemonNames.forEach((pokemonName) => {
+            requests.push(fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(response => response.json()))
+        })
+        Promise.all(requests).then((pokemonArray) => {
+            result = pokemonArray.map((pokemonJson) => {
+                return { 'name': pokemonJson.species.name, 'type': pokemonJson.types.map((type) => type.type.name).reduce((acc, type) => `${acc}, ${type}`) }
+            })
+            resolve(result)
+        })
+    })
+}
+let printTypes = () => {
+    let types = getTypes()
+    types.then((types) => types.forEach((type) => {
+        console.log(`${type.name}: ${type.type}`)
+    }))
+}
+module.exports = { getTypes, printTypes }
